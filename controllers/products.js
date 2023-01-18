db29bf8c578520007407d492c65f6c681a37ab1b
Hafid Qarchi <haqarchi@gmail.com>
2023-01-18 17:25:39 +0900

    config express async error package to handle custom error

--- controllers/products.js
+++ controllers/products.js
@@ -1,4 +1,5 @@
 const getAllProducts = async (req, res) => {
+  throw new Error(`Cannot get all products`);
   res.status(200).json({ msg: "all products " });
 };
 const getAllProductsStatic = async (req, res) => {
