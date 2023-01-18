8932ef306dcb8dd46d99affaf6a130bfbc276cdd
Hafid Qarchi <haqarchi@gmail.com>
2023-01-18 16:46:27 +0900

    basic routes in app

--- app.js
+++ app.js
@@ -9,7 +9,23 @@ import productsRouter from "./routes/products.js";
 
 const PORT = process.env.PORT || 3000;
 const connectionString = process.env.MONGO_DB_CONNECTION;
-console.log(connectionString);
+
+// middleware
+app.use(express.json());
+
+// routes
+// app.use(productsRouter);
+app.get("/", (req, res) => {
+  res.send(`<h1>Store API</h1> <a href="/api/v1/products">Products</a>`);
+});
+
+// products route
+app.get("/api/v1/products", (req, res) => {
+  res.send(`<h1>Products</h1> <a href="/">Back</a>`);
+});
+
+app.use(notFound);
+app.use(errorHandler);
 
 const start = async () => {
   try {
