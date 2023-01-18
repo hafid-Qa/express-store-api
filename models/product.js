c1f2223b8570bd77de4d9e09dfeec98d70282bbd
Hafid Qarchi <haqarchi@gmail.com>
2023-01-18 20:16:37 +0900

    db schema defined

---
+++ models/product.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Product name is required"],
+  },
+  price: {
+    type: Number,
+    required: [true, "Product price is required"],
+  },
+  featured: {
+    type: Boolean,
+    default: false,
+  },
+  company: {
+    type: String,
+    enum: {
+      values: ["EK", "EY", "QR", "AT"],
+      message: "{VALUE} is not a valid company name",
+    },
+    // enum: ["EK", "EY", "QR", "AT"],
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  createdAT: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+export default mongoose.model("Product", productSchema);
