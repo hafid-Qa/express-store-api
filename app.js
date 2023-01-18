35b4d5a0a1e563a3940e0cb7de62723a7509bfd9
Hafid Qarchi <haqarchi@gmail.com>
2023-01-18 16:02:27 +0900

    import all require modules to app

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ import express from "express";
 const app = express();
 import * as dotenv from "dotenv";
 dotenv.config();
+import connectDB from "./db/connect.js";
+import notFound from "./middleware/not-found.js";
+import errorHandler from "./middleware/error-handler.js";
+import productsRouter from "./routes/products.js";
 
 const PORT = process.env.PORT || 3000;
 const connectionString = process.env.MONGO_DB_CONNECTION;
 console.log(connectionString);
-
 
 const start = async () => {
   try {
