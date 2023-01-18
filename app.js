e0758acb1fb8e4f5fe36837a159792892e5468eb
Hafid Qarchi <haqarchi@gmail.com>
2023-01-18 15:55:06 +0900

    app init setup

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log('04 Store API')
+import express from "express";
+const app = express();
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+const connectionString = process.env.MONGO_DB_CONNECTION;
+console.log(connectionString);
+
+
+const start = async () => {
+  try {
+    await connectDB(connectionString);
+    app.listen(PORT, console.log(`Listening on port ${PORT}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
