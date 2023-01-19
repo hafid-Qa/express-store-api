78e99f046dca5b8e41c19f7f26cb14283a9c2565
Hafid Qarchi <haqarchi@gmail.com>
2023-01-19 16:33:09 +0900

    get product based on query params

--- controllers/products.js
+++ controllers/products.js
@@ -1,15 +1,15 @@
 import Product from "../models/product.js";
 
 const getAllProducts = async (req, res) => {
-  throw new Error(`Cannot get all products`);
-
-  res.status(200).json({ msg: "all products " });
+  const products = await Product.find(req.query);
+  res.status(200).json({
+    numberOfProducts: products.length,
+    products,
+  });
 };
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({
-    featured: true,
-  });
-  res.status(200).json({ products, nbHits: products.length });
+  const products = await Product.find({});
+  res.status(200).json({ numberOfProducts: products.length, products });
 };
 
 export { getAllProducts, getAllProductsStatic };
