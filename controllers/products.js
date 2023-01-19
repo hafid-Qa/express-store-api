20eb6db2f6d42b569701740f25e5a0e98cb9024d
Hafid Qarchi <haqarchi@gmail.com>
2023-01-19 17:08:30 +0900

    get product based on 3 params

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,19 @@
 import Product from "../models/product.js";
 
 const getAllProducts = async (req, res) => {
-  const products = await Product.find(req.query);
+  const { featured, rating, name, price, company } = req.query;
+  const queryObject = {};
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+  if (company) {
+    queryObject.company = company.toUpperCase();
+  }
+  if (name) {
+    queryObject.name = name.toLowerCase();
+  }
+  console.log(queryObject);
+  const products = await Product.find(queryObject);
   res.status(200).json({
     numberOfProducts: products.length,
     products,
